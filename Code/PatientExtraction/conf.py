f12cc8c9fd37a00e5b4c21708f3d53e12ecca9bb
sb0080 <s.c.bull@surrey.ac.uk>
2016-09-08 13:53:09 +0100

    Converted all value1 to val1 and value2 to val2

--- Code/PatientExtraction/conf.py
+++ Code/PatientExtraction/conf.py
@@ -16,17 +16,26 @@ def init(args=None):
     """Initialise the settings-like variables."""
 
     # Initialise the dictionary of valid modes, outputs and operators that can appear in a case definition file.
-    validModes = {"all": record_selector.all_selector, "earliest": record_selector.earliest_selector,
-                  "latest": record_selector.latest_selector, "max1": record_selector.max_selector("Val1"),
-                  "max2": record_selector.max_selector("Val2"), "min1": record_selector.min_selector("Val1"),
+    validModes = {"all": record_selector.all_selector,
+                  "earliest": record_selector.earliest_selector,
+                  "latest": record_selector.latest_selector,
+                  "max1": record_selector.max_selector("Val1"),
+                  "max2": record_selector.max_selector("Val2"),
+                  "min1": record_selector.min_selector("Val1"),
                   "min2": record_selector.min_selector("Val2")}  # The valid code selection modes.
-    validOutputs = {"code": record_outputter.code_outputter, "count": record_outputter.count_outputter,
-                    "date": record_outputter.date_outputter, "max1": record_outputter.max_outputter("Val1"),
-                    "max2": record_outputter.max_outputter("Val2"), "mean1": record_outputter.mean_outputter("Val1"),
-                    "mean2": record_outputter.mean_outputter("Val2"), "median1": record_outputter.median_outputter("Val1"),
-                    "median2": record_outputter.median_outputter("Val2"), "min1": record_outputter.min_outputter("Val1"),
-                    "min2": record_outputter.min_outputter("Val2"), "value1": record_outputter.value_outputter("Val1"),
-                    "value2": record_outputter.value_outputter("Val2")}  # The valid output options.
+    validOutputs = {"code": record_outputter.code_outputter,
+                    "count": record_outputter.count_outputter,
+                    "date": record_outputter.date_outputter,
+                    "max1": record_outputter.max_outputter("Val1"),
+                    "max2": record_outputter.max_outputter("Val2"),
+                    "mean1": record_outputter.mean_outputter("Val1"),
+                    "mean2": record_outputter.mean_outputter("Val2"),
+                    "median1": record_outputter.median_outputter("Val1"),
+                    "median2": record_outputter.median_outputter("Val2"),
+                    "min1": record_outputter.min_outputter("Val1"),
+                    "min2": record_outputter.min_outputter("Val2"),
+                    "val1": record_outputter.value_outputter("Val1"),
+                    "val2": record_outputter.value_outputter("Val2")}  # The valid output options.
     validOperators = {'>': operator.gt, ">=": operator.ge,
                       '<': operator.lt, "<=": operator.le}  # The valid value restriction operators.
     global validChoices
